cd3c75a5643f96782417022bbea92bee08e26725
cazyw <kazeisc@gmail.com>
2018-07-21 15:37:44 +1000

    Update sendgrid settings

--- config/plugins/wordpress/wordpress-sendgrid.php
+++ config/plugins/wordpress/wordpress-sendgrid.php
@@ -7,9 +7,13 @@ if (!empty(getenv('SENDGRID_API_KEY'))) {
     // Auth method ('apikey')
     define('SENDGRID_AUTH_METHOD', 'apikey');
     define('SENDGRID_API_KEY', getenv('SENDGRID_API_KEY'));
-} else if (!empty(getenv('SENDGRID_USERNAME')) && !empty(getenv('SENDGRID_PASSWORD'))) {
-    // Auth method ('credentials')
-    define('SENDGRID_AUTH_METHOD', 'credentials');
-    define('SENDGRID_USERNAME', getenv('SENDGRID_USERNAME'));
-    define('SENDGRID_PASSWORD', getenv('SENDGRID_PASSWORD'));
 }
+
+// the below no longer appears to work - an api key is required
+
+// } else if (!empty(getenv('SENDGRID_USERNAME')) && !empty(getenv('SENDGRID_PASSWORD'))) {
+//     // Auth method ('credentials')
+//     define('SENDGRID_AUTH_METHOD', 'credentials');
+//     define('SENDGRID_USERNAME', getenv('SENDGRID_USERNAME'));
+//     define('SENDGRID_PASSWORD', getenv('SENDGRID_PASSWORD'));
+// }
